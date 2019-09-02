3485d8dba1d5c70b126bce336a88b506f6692ff2
AkagiShiroe <kirihaasuka@gmail.com>
2019-09-02 18:50:10 +0900

    derp code

--- drivers/net/ethernet/msm/rndis_ipa_trace.h
+++ drivers/net/ethernet/msm/rndis_ipa_trace.h
@@ -11,8 +11,8 @@
  */
 
 #undef TRACE_SYSTEM
-#define TRACE_SYSTEM drivers/net/ethernet/msm/rndis_ipa
-#define TRACE_INCLUDE_FILE drivers/net/ethernet/msm/rndis_ipa_trace
+#define TRACE_SYSTEM rndis_ipa
+#define TRACE_INCLUDE_FILE netrndis_ipa_trace
 
 #if !defined(_RNDIS_IPA_TRACE_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _RNDIS_IPA_TRACE_H
@@ -76,6 +76,6 @@ TRACE_EVENT(
 #endif /* _RNDIS_IPA_TRACE_H */
 
 /* This part must be outside protection */
-#undef TRACE_INCLUDE_PATH
-#define TRACE_INCLUDE_PATH .
+ 	#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH ../../drivers/net/ethernet/msm
 #include <trace/define_trace.h>
