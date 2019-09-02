8269e65d6bcc554c612fed81b38bf5ab7753f4c2
AkagiShiroe <kirihaasuka@gmail.com>
2019-09-02 13:08:45 +0700

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,7 +1,11 @@
+export CONFIG_FILE="$TRAVIS_BUILD_DIR/arch/arm64/octopus_defconfig"
 export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE="./gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-" ;
 rm -rf out
 mkdir -p out
-make O=out octupus_defconfig
-make O=out -j16
+JOBS="-j$(nproc --all)"
+make O= $TRAVIS_BUILD_DIR/arch/arm64/configs/octopus_defconfig
+make \
+	O=${out_dir} \
+	$JOBS
